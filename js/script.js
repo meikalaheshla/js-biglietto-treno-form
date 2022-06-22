cb0c015ffc86e801fc49172de04d237b869191e6
meikalaheshla <fausto.boshoff@gmail.com>
2022-06-22 19:54:03 +0200

    problem with travelLenght not taking input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 
-const fullName = document.getElementById('fullname').value
+const fullName = document.getElementById('fullname')
 const travelLenght = parseInt(document.getElementById('kmtodo').value)
-const travelerAge = document.getElementById('age-groups').value
+const travelerAge = document.getElementById('age-groups')
 let TicketPrice = travelLenght * 0.21
 const btnGenerate = document.getElementById('genera')
 const btnCancel = document.getElementById('annulla')
@@ -11,32 +11,36 @@ const nameDisplayer = document.getElementById('name-tab')
 const priceDisplayer = document.getElementById('price-tab')
 const offeringDsiplayer = document.getElementById('offerta')
 console.log(fullName, travelLenght, travelerAge, TicketPrice)
+console.log(typeof TicketPrice)
 
 
 
 console.log(TicketPrice)
 
 btnGenerate.addEventListener('click', function () {
-    if (travelerAge === 'minorenne') {
+    if (travelerAge.value === 'minorenne') {
         const youngDiscount = TicketPrice * 0.20
         TicketPrice = TicketPrice - youngDiscount
         offeringDsiplayer.innerText = `offerta giovani`
 
         console.log(youngDiscount)
+        console.log(typeof TicketPrice)
 
-    } else if (travelerAge === 'oversixtyfive') {
+    } else if (travelerAge.value === 'oversixtyfive') {
         const oldDiscount = TicketPrice * 0.40
         TicketPrice = TicketPrice - oldDiscount
         offeringDsiplayer.innerText = `offerta over 65`
 
         console.log(oldDiscount)
+        console.log(typeof TicketPrice)
 
 
-    } else {
-        offeringDsiplayer.innerText = `biglietto intero`
+        // } else {
+        //     offeringDsiplayer.innerText = `biglietto intero`
     }
-    nameDisplayer.innerText = fullName
+    nameDisplayer.innerText = fullName.value
     priceDisplayer.innerText = `€ ${TicketPrice}`
+    console.log(typeof TicketPrice)
 
 
 
